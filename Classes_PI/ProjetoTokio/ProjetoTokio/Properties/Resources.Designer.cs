42dd3309627985efe3d70ecf0a2bcdce93eb1ea1
rmayara <mayara.borges011@gmail.com>
2021-05-21 19:15:31 -0300

    ajuste cor tabu

--- Classes_PI/ProjetoTokio/ProjetoTokio/Properties/Resources.Designer.cs
+++ Classes_PI/ProjetoTokio/ProjetoTokio/Properties/Resources.Designer.cs
@@ -153,11 +153,6 @@ namespace ProjetoTokio.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
-        
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap baseVerde {
             get {
                 object obj = ResourceManager.GetObject("baseVerde", resourceCulture);
